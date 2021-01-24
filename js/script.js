4a18f47dcf7de622909224fb8c5e3a316692be25
Connor O'Brien <connor@hifranc.com>
2021-01-25 02:06:09 +0400

    Add answers

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Question 1
+let anything;
+let off;
+let word;
+let total;
+// Question 2
+let booleans;
+// or
+let myBooleans;
+// Question 3
+let twoTypes;
+// Question 4
+let tuple;
---
+++ ts/script.ts
@@ -0,0 +1,16 @@
+// Question 1
+let anything: any;
+let off: boolean;
+let word: string;
+let total: number;
+
+// Question 2
+let booleans: boolean[];
+// or
+let myBooleans: Array<boolean>;
+
+// Question 3
+let twoTypes: (string | number)[];
+
+// Question 4
+let tuple: [string, string, number];
